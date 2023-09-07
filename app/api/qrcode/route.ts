1ac4379f5b45fd61c83590c9abb7f16986bdf5ac
Zhen Yang <webyangfullstack@gmail.com>
2023-09-07 18:40:06 +1000

    create qrCode on edit page

--- app/api/qrcode/route.ts
+++ app/api/qrcode/route.ts
@@ -6,9 +6,9 @@ import axios from 'axios';
 export async function POST(req: NextRequest) {
     const body = await req.json()
     const { surveyId, imageUrl } = body
-    const data = await axios.post('https://api.qr.io/v1/create', JSON.stringify({
-        "apikey": "V69JS1WY08vEXK5eUmir",
-        "data": "https://www.edgecademy.org",
+    const data1 = {
+        "apikey": `${NEXT_QR_API_KEY}`,
+        "data": `${BASE_URL}/survey/${surveyId}`,
         "transparent": "off",
         "backcolor": "#033895",
         "frontcolor": "#ffffff",
@@ -18,12 +18,19 @@ export async function POST(req: NextRequest) {
         "marker": "flower",
         "marker_in": "circle",
         "optionlogo": "https://cdn.sanity.io/images/er52rfe6/production/2d919dd0e2bf59d69e48ec04e15657bad6fff3c3-1280x1706.jpg"
-    }))
+      }
+      const data = await axios.post('https://api.qr.io/v1/create', JSON.stringify(data1), {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json', // Set the Content-Type header
+        },
+      })
+      console.log(data.data)
 
     return NextResponse.json({
         success: true,
         status: 200,
         message: "Success",
-        data: data,
+        data: data.data,
     })
 }
--- app/survey/[slug]/page.tsx
+++ app/survey/[slug]/page.tsx
@@ -19,24 +19,25 @@ export default function Page({ params }: { params: { slug: string } }) {
     console.log(result)
     setItem(result)
   }, [params.slug])
+  const [qrCodeUrl, setQRCodeUrl] = useState<string>('')
   const generateQRCode = async () => {
-    // const { data } = await axios.post(`${BASE_URL}/api/qrcode`, { surveyId: params.slug, imageUrl: item?.image.asset.url })
-    // console.log(data)
-    const data1 ={
-      "apikey": "V69JS1WY08vEXK5eUmir",
-      "data": "https://www.edgecademy.org",
-      "transparent": "off",
-      "backcolor": "#033895",
-      "frontcolor": "#ffffff",
-      "marker_out_color": "#669df4",
-      "marker_in_color": "#669df4",
-      "pattern": "oriental",
-      "marker": "flower",
-      "marker_in": "circle",
-      "optionlogo": "https://cdn.sanity.io/images/er52rfe6/production/2d919dd0e2bf59d69e48ec04e15657bad6fff3c3-1280x1706.jpg"
-  }
-    const data = await axios.post('https://api.qr.io/v1/create', JSON.stringify(data1))
-    console.log(data)
+    const { data } = await axios.post(`${BASE_URL}/api/qrcode`, { surveyId: params.slug, imageUrl: item?.image.asset.url })
+    console.log(data.data.png)
+    setQRCodeUrl(data.data.png)
+    // update the survey with qrcode in survey store
+    const result = survey.map((item: Survey) => {
+      if (item._id === params.slug) {
+        return {
+          ...item, qrCode: {
+            asset: {
+              url: data.data.png
+            }
+          }
+        }
+      }
+      return item
+    })
+    updateSurvey(result)
   }
   return (
     <>
@@ -45,7 +46,8 @@ export default function Page({ params }: { params: { slug: string } }) {
           <div className='text-white bg-black bg-opacity-30 backdrop-filter backdrop-blur-lg shadow-lg rounded-lg w-full h-full overflow-auto p-5 relative max-w-[800px] mx-auto overflow-y-auto'>
             <BiLeftArrowAlt className="absolute top-5 sm:top-4 left-5 text-2xl sm:text-3xl cursor-pointer hover:text-blue-400 transition-all ease-in-out duration-150" onClick={() => router.push('/')} />
             <h1 className='text-center font-bold'>{item.title} <span className='text-gray-300 font-normal'>{item.createdAt.slice(0, 10)}</span></h1>
-            <div className='flex flex-row gap-5 w-full'>
+            <div className='flex flex-col gap-5 w-full justify-center items-center mt-10'>
+              {qrCodeUrl && <img src={qrCodeUrl} alt="" className='w-[200px]'/>}
               <Button onClick={() => generateQRCode()} className='text-white'>Generate QR Code</Button>
             </div>
           </div>
