888e7d38f0e9a01a5db67204e65cd1a6615622f9
Zhen Yang <webyangfullstack@gmail.com>
2023-09-08 18:48:19 +1000

    remove title

--- app/form/[slug]/page.tsx
+++ app/form/[slug]/page.tsx
@@ -160,7 +160,7 @@ export default function Page({ params }: { params: { slug: string } }) {
         item && !loading ? <div className='w-screen  bg-cover px-5 py-10' style={{ backgroundImage: `url(${item.image.asset.url})` }}>
           <div className='text-white bg-black bg-opacity-30 backdrop-filter backdrop-blur-lg shadow-lg rounded-lg w-full h-full  p-5 relative max-w-[800px] mx-auto'>
             {/* <BiLeftArrowAlt className="absolute top-5 sm:top-4 left-5 text-2xl sm:text-3xl cursor-pointer hover:text-blue-400 transition-all ease-in-out duration-150" onClick={() => router.push('/')} /> */}
-            <h1 className='text-center font-bold'>{item.title} {item.createdAt.slice(0, 10)} <span className='text-gray-300 font-normal'>{` `} Preview</span></h1>
+            {/* <h1 className='text-center font-bold'>{item.title} {item.createdAt.slice(0, 10)} <span className='text-gray-300 font-normal'>{` `} Preview</span></h1> */}
             <Form
               name="landing_signup"
               initialValues={{ remember: true }}
